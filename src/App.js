eaa2420baa6675f5973f892128765e934b1e7c06
Savanna Goring <savannagoring@Savannas-MacBook-Air.local>
2018-02-13 01:03:49 -0800

    added link

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
         
     }
     componentDidMount(){
-       // this.socket = mySocket("http://localhost:10001");
+     this.socket = mySocket("https://interactiveworld.herokuapp.com/");
         
     }
     
